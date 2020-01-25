75760542bdda424bcefcaa6e20643fb3ef1bad38
Saltibarsciai <rakunas.dominykas@gmail.com>
2020-01-25 18:33:35 +0200

    creat dummy images and ImageContainer design

--- src/components/ImagesContainer/ImagesContainer.js
+++ src/components/ImagesContainer/ImagesContainer.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import "./ImagesContainer.scss"
+import logo from "../../invega.jpg"
 
 const ImagesContainer = () => {
-
+    return (
+        <main className="images-container">
+            <img src={logo} alt="placeholder"/>
+            <img src={logo} alt="placeholder"/>
+            <img src={logo} alt="placeholder"/>
+            <img src={logo} alt="placeholder"/>
+            <img src={logo} alt="placeholder"/>
+            <img src={logo} alt="placeholder"/>
+        </main>
+    );
 };
 
 export default ImagesContainer;
