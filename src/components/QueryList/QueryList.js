8527742dc30b4e6bbd5bffd68449b8f4db9bc28b
Saltibarsciai <rakunas.dominykas@gmail.com>
2020-01-25 18:31:57 +0200

    create list of keywords + select and fetch functionallity

--- src/components/QueryList/QueryList.js
+++ src/components/QueryList/QueryList.js
@@ -1,8 +1,42 @@
 import React, {useState} from 'react';
-
+import {SlideDown} from 'react-slidedown'
+import 'react-slidedown/lib/slidedown.css'
+import "./QueryList.scss"
+import {useSelector, useDispatch} from "react-redux";
+import UnsplashFactory from "../../utils/Unsplash";
+import {addImages} from "../../store/actions"
 
 const QueryList = () => {
+    const [isClosed, setClosed] = useState(true);
+    const queryStore = useSelector(state => state.queryReducer);
+    const dispatch = useDispatch();
 
+    const dispatchImages = (item) => {
+        UnsplashFactory
+            .fetch(item)
+            .then( results => dispatch(addImages(results)));
+    };
+
+    const queryList = () => {
+        return (
+            queryStore.map((item, index) => (
+                <li key={index} onClick={ () => dispatchImages(item)}>
+                    {item}
+                </li>
+            ))
+        )
+    };
+
+    return (
+        <section className={"query-list"}>
+            <button onClick={() => setClosed(!isClosed)}>
+                Show saved queries
+            </button>
+            <SlideDown className="slider" closed={isClosed}>
+                {queryList()}
+            </SlideDown>
+        </section>
+    );
 };
 
 export default QueryList;
